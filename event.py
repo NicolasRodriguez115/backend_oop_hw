8d952e893ff61590587fe322677f68d9ff6a29d9
NicolasRodriguez115 <nr115935@gmail.com>
2024-06-26 16:40:01 -0400

    Added  more vehicles and changed counter parameter to attendance

--- event.py
+++ event.py
@@ -1,11 +1,11 @@
 class Event:
-    def __init__(self, name, date, counter):
+    def __init__(self, name, date, attendance):
         self.name = name
         self.date = date
-        self.counter = counter
+        self.attendance = attendance
 
     def add_participant(self, new_participant):
-        self.counter += new_participant
+        self.attendance += new_participant
 
     def get_participant_count(self):
         print(f"There are currently {self.counter} participants at the event")
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from vehicle import Vehicle
 from event import Event
-vehicle = Vehicle("7AKN145", "Toyota", "Nicolas")
-vehicle.display_info()
-vehicle.update_owner("Andres")
-vehicle.display_info()
+veh_1 = Vehicle("7AKN145", "Toyota", "Nicolas")
+veh_1.display_info()
+veh_1.update_owner("Andres")
+veh_1.display_info()
+
+veh_2 = Vehicle("9GIO426", "Kia", "Dakota")
+veh_2.display_info()
+veh_2.update_owner("Deacon")
+veh_2.display_info()
+
+
+veh_3 = Vehicle("3TRP527", "BMW", "Jhon")
+veh_3.display_info()
+veh_3.update_owner("Paul")
+veh_3.display_info()
 
 event = Event("Nicolas", "6/25/2024", 1)
 event.add_participant(5)
